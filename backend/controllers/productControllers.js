130f3e3785d7e28b00104f7b778ff847fe43cef3
sagarmintu <sagarskbehera1996@gmail.com>
2026-03-03 20:27:56 +0530

    retrieve product data

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,10 +1,12 @@
 import Product from '../models/product.js';
 
 // Retrieve products => /api/v1/products
-export const getProducts = (req, res) => {
+export const getProducts = async (req, res) => {
+    const products = await Product.find(req.body);
     res.status(200).json({
         status: 'success',
-        message: 'Products fetched successfully'
+        message: 'Products fetched successfully',
+        products: products,
     });
 }
 
