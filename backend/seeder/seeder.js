c20534b0f6edef68eecb8b0b08b47ff491a97f36
sagarmintu <sagarskbehera1996@gmail.com>
2026-03-03 20:45:20 +0530

    creating data seeder

---
+++ backend/seeder/seeder.js
@@ -0,0 +1,25 @@
+import Mongoose from "mongoose";
+import products from "./data.js";
+import Product from "../models/product.js";
+
+const seedProducts = async () => {
+    try 
+    {
+        await Mongoose.connect("mongodb://localhost:27017/shopping");
+
+        await Product.deleteMany();
+        console.log("Products are deleted");
+
+        await Product.insertMany(products);
+        console.log("Products are added");
+
+        process.exit();
+    } 
+    catch (error) 
+    {
+        console.log(error.message);
+        process.exit();
+    }
+}
+
+seedProducts();
