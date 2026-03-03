0733facc628db60c796c993c54974180088aaf6a
sagarmintu <sagarskbehera1996@gmail.com>
2026-03-03 19:34:11 +0530

    Admin create&save products

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,6 +1,19 @@
+import Product from '../models/product.js';
+
+// Retrieve products => /api/v1/products
 export const getProducts = (req, res) => {
     res.status(200).json({
         status: 'success',
         message: 'Products fetched successfully'
+    });
+}
+
+// Create a new product => /api/v1/admin/products
+export const newProduct = async (req, res) => {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        status: 'success',
+        message: 'Product created successfully',
+        product,
     });
 }
